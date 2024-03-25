00f5355d503bd9d94b0469753780e1f1e8557e77
itsmeabhi12 <abhishek.ghimire281@gmail.com>
2024-03-25 11:16:32 +0545

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by ebpearls on 25/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
